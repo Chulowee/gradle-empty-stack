6970c8fbf78446661780d745b9e7848ca11a21bf
chulowee <katrina.chloe.reyes@oocl.com>
2019-10-10 14:54:28 +0800

    Updated Limit

--- src/main/java/model/iPhone.java
+++ src/main/java/model/iPhone.java
@@ -1,10 +1,10 @@
 package model;
 
 public class iPhone extends Mobile {
-    private int limit = 6;
+    private int limit = 10;
     @Override
     public String call(String message){
-        if (message.length() >12){
+        if (message.length() >limit){
             return "<iPhone> Message cannot be sent";
         }
         return "<iPhone>Message : "+ message;
