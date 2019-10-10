1c984d14eaccd0bf953e130d146d6edfbb109958
chulowee <katrina.chloe.reyes@oocl.com>
2019-10-10 14:40:07 +0800

    Created iPhone

--- src/main/java/model/iPhone.java
+++ src/main/java/model/iPhone.java
@@ -1,8 +1,13 @@
 package model;
 
 public class iPhone extends Mobile {
+    private int limit = 6;
     @Override
     public String call(String message){
+        if (message.length() >12){
+            return "<iPhone> Message cannot be sent";
+        }
         return "<iPhone>Message : "+ message;
     }
+
 }
