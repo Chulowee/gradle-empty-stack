1c2eb2b80cfd488b6350670b809f84278ad85b57
chulowee <katrina.chloe.reyes@oocl.com>
2019-10-10 14:55:08 +0800

    added getter and setters

--- src/main/java/model/Mobile.java
+++ src/main/java/model/Mobile.java
@@ -1,11 +1,40 @@
 package model;
 
-public class Mobile {
+public abstract class Mobile {
     private String name;
     private String color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     private String brand;
 
     public String call(String message) {
         return "Message : "+ message;
+    }
+
+    public String info(){
+        return "name: "+ getName() + ", color: "+getColor()+", brand: "+getBrand();
     }
 }
