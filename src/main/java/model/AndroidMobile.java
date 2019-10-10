5dc04a9c64ab140746321934e986965803333d09
chulowee <katrina.chloe.reyes@oocl.com>
2019-10-10 14:39:50 +0800

    Created AndroidMobile

--- src/main/java/model/AndroidMobile.java
+++ src/main/java/model/AndroidMobile.java
@@ -1,8 +1,14 @@
 package model;
 
 public class AndroidMobile extends Mobile {
+    private int limit = 6;
     @Override
     public String call(String message){
+        if (message.length()>=limit){
+            return "<Andriod>Message cannot be sent";
+        }
         return "<Andriod>Message : "+ message;
     }
+
+
 }
